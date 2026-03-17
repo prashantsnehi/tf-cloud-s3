e3b879403f06cc3f91951ad457306bd4a9d1f8d4
Pointpal India <prashant@pointpalindia.com>
2026-03-17 22:03:20 +0530

    for loop for instances in output

--- output.tf
+++ output.tf
@@ -3,15 +3,15 @@ output "bucket_name" {
 }
 
 output "instance_id" {
-  value = aws_instance.myserver.id
+  value = [for idx, instance in aws_instance.myserver : "${idx}-${instance.id}"]
 }
 
 output "instance_private_ip" {
-  value = aws_instance.myserver.private_ip
+  value = [for idx, instance in aws_instance.myserver : "${idx}-${instance.id}:-${instance.private_ip}"]
 }
 
 output "instance_public_ip" {
-  value = aws_instance.myserver.public_ip
+  value = [for idx, instance in aws_instance.myserver : "${idx}-${instance.id}:-${instance.public_ip}"]
 }
 
 output "vpc_id" {
@@ -19,5 +19,5 @@ output "vpc_id" {
 }
 
 output "subnet_id" {
-  value = [for idx,subnet in aws_subnet.subnets : "${idx}:-${subnet.id}" ]
+  value = [for idx, subnet in aws_subnet.subnets : "${idx}:-${subnet.id}"]
 }
