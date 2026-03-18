b6a67c5b55ef5383c4602d198860cfe255d7c099
Pointpal India <prashant@pointpalindia.com>
2026-03-18 17:09:49 +0530

    updated region

--- variable.tf
+++ variable.tf
@@ -20,6 +20,7 @@ variable "aws_instances_details" {
     name              = string
     instance_type     = string
     ami               = string
+    region            = string 
     availability_zone = string
   }))
 }
